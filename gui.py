14bc2013b8e7cf2954ff69e8f5e37af71e73fc5f
Renato Britto <renatobritto@protonmail.com>
2023-04-13 11:06:00 -0300

    qui 13 abr 2023 11:06:00 -03

--- gui.py
+++ gui.py
@@ -2,13 +2,28 @@ import pygame
 import datetime
 import time
 import json
-from math import min, max
+from enum import Enum
+import traceback
+
+DEBUG = True
+def dprint(*values: object):
+    print("[DEBUG]", *values)
+
+def eprint(*values: object):
+    traceback.print_stack()
+    print("[ERROR]", *values)
+
+def enum_list(values: Enum):
+    r = []
+    for value in values:
+        r.append(value.name)
+    return sorted(r)
 
 point = list[int, int]
-direcao = str["leste", "oeste", "norte", "sul", "parado"]
-faixa_tipo = str["acostamento", "geral"]
+cor = tuple[int, int, int]
 
-cor = tuple(int, int, int)
+Direcao = Enum('Direcao', "leste oeste norte sul parado")
+FaixaTipo = Enum('FaixaTipo', "acostamento geral")
 
 COR_ACOSTAMENTO = (30, 30, 30)
 COR_FAIXA = (15, 15, 15)
@@ -17,6 +32,31 @@ COR_DIVISORIA_FAIXA_SENTIDO_DIFERENTE = (200, 200, 0)
 COR_DIVISORIA_FAIXA_ACOSTAMENTO = (160, 160, 160)
 
 LARGURA_DIVISORIA = 2
+
+
+class DrawItem():
+    def __init__(self):
+        pass
+
+    def draw(self, scr: pygame.Surface):
+        eprint("FAILED TO IMPLEMENT DRAW")
+        exit(1)
+
+
+class Drawer():
+    x: int = 0
+    draw_items: list[DrawItem]
+
+    def __init__(self, draw_items: list[DrawItem] = []):
+        self.draw_items = draw_items
+
+    def draw(self, scr: pygame.Surface):
+        scr.fill((255, 255, 255))
+
+        for draw_item in self.draw_items:
+            draw_item.draw(scr)
+
+        pygame.display.flip()
 
 
 class GUI():
@@ -28,14 +68,11 @@ class GUI():
     running = False
     drawer = None
 
-    def __init__(self):
-        pass
-
-    def init(self):
+    def __init__(self, drawer: Drawer = Drawer()):
         pygame.init()
         self.scr = pygame.display.set_mode((600, 500))
         self.running = True
-        self.drawer = Drawer()
+        self.drawer = drawer
 
     def run(self):
         while self.running:
@@ -58,53 +95,25 @@ class GUI():
         pygame.quit()
 
 
-class DrawItem():
-    def __init__(self):
-        pass
-
-    def draw(self, scr: pygame.Surface):
-        print("FAILED TO IMPLEMENT DRAW")
-        exit(1)
-
-
-class Drawer():
-    x: int = 0
-    draw_items: list[DrawItem]
-
-    def __init__(self):
-        pass
-
-    def draw(self, scr: pygame.Surface):
-        scr.fill((255, 255, 255))
-
-        pygame.draw.circle(scr, (200, 0, 0), (self.x, 250), 80)
-        pygame.display.flip()
-
-        self.x = self.x + 1
-
-        if self.x > 600-250:
-            self.x = 250/2.0
-
-
 class Faixa():
-    tipo: faixa_tipo
-    direcao_de_movimento: direcao
-    sentido: direcao
+    tipo: FaixaTipo
+    direcao_de_movimento: Direcao
+    sentido: Direcao
 
-    def __init__(self, name, relativo, sentido):
-        self.name = name
-        self.relativo = relativo
-        self.sentido = sentido
+    def __init__(self, tipo: FaixaTipo, relativo: Direcao, sentido: Direcao):
+        self.tipo = FaixaTipo(tipo)
+        self.relativo = Direcao(relativo)
+        self.sentido = Direcao(sentido)
 
 
 class Pista():
     p1: point
     p2: point
-    direcao: direcao
+    direcao: Direcao
     faixas: list[Faixa]
     step_w: int = None
 
-    def __init__(self, p1: point, p2: point, direcao: direcao, faixas: list[Faixa]):
+    def __init__(self, p1: point, p2: point, direcao: Direcao, faixas: list[Faixa]):
         self.p1 = p1
         self.p2 = p2
         self.direcao = direcao
@@ -119,10 +128,13 @@ def rect(p1: point, p2: point):
     return (x, y, w, h)
 
 
-def get_params_directional_rect(p1: point, p2: point, direcao: direcao, dlt: float):
-    x, y, w, h: float
+def get_params_directional_rect(p1: point, p2: point, direcao: Direcao, dlt: float):
+    x: float
+    y: float
+    w: float
+    h: float
 
-    if direcao == "leste" or direcao == "oeste":
+    if direcao == Direcao["leste"] or direcao == Direcao["oeste"]:
         x, y, w, h = rect(
             [p1[0], p1[1] + dlt], [p2[0], p2[1] + dlt]
         )
@@ -134,26 +146,6 @@ def get_params_directional_rect(p1: point, p2: point, direcao: direcao, dlt: flo
     return (x, y, w, h)
 
 
-class FaixaDrawer(DrawItem):
-    faixa: Faixa = None
-
-    def __init__(self, faixa: Faixa):
-        self.faixa = faixa
-
-    def draw(self, dlt: float, scr: pygame.Surface, p1, p2, direcao: direcao) -> float:
-        faixa = self.faixa
-
-        cor = COR_FAIXA
-        if self.tipo == "acostamento":
-            cor = COR_ACOSTAMENTO
-
-        # botar setas pra indicar direção?
-
-        rect = get_params_directional_rect(p1, p2, direcao, dlt)
-
-        pygame.draw.rect(scr, cor, rect)
-
-
 class PistaDrawer(DrawItem):
     pista: Pista = None
 
@@ -163,35 +155,38 @@ class PistaDrawer(DrawItem):
     def draw(self, scr: pygame.Surface):
         last_faixa = None
         dlt = 0
-        for faixa in self.faixas:
+        for faixa in self.pista.faixas:
             if last_faixa is not None:
                 dlt = dlt + \
                     self.draw_divisoria(
-                        dlt, scr, last_faixa, faixa, self.direcao)
+                        dlt, scr, last_faixa, faixa, self.pista.direcao)
 
-            faixa_drawer = FaixaDrawer(faixa)
-            dlt = dlt + faixa_drawer.draw(scr)
+            dlt = dlt = self.draw_faixa(dlt, scr, faixa)
 
             last_faixa = faixa
 
-    def get_cor_divisoria(faixa_anterior: Faixa, faixa_proxima: Faixa) -> cor:
-        if faixa_anterior.tipo != "acostamento" and faixa_proxima.tipo == "acostamento":
+    def get_cor_divisoria(self, faixa_anterior: Faixa, faixa_proxima: Faixa) -> cor:
+        tipos = enum_list([faixa_anterior.tipo, faixa_proxima.tipo])
+        if tipos == ["acostamento", "geral"]:
+            dprint(1)
             return COR_DIVISORIA_FAIXA_ACOSTAMENTO
-        elif faixa_anterior.tipo == "acostamento" and faixa_proxima.tipo == "acostamento":
-            return COR_DIVISORIA_FAIXA_ACOSTAMENTO
-        elif faixa_anterior.tipo == "acostamento" and faixa_proxima.tipo == "acostamento":
-            print("ERRO: não pode ter acostamento seguido de acostamento")
+        elif tipos == ["acostamento", "acostamento"]:
+            dprint(2)
+            eprint("acostamento seguido de acostamento")
             exit(1)
         elif faixa_anterior.tipo == faixa_proxima.tipo:
+            dprint(3)
             return COR_DIVISORIA_FAIXA_SENTIDO_IGUAL
-        elif faixa_anterior.direcao_de_movimento != faixa_anterior.direcao_de_movimento:
+        elif faixa_anterior.sentido != faixa_anterior.sentido:
+            dprint(4)
             return COR_DIVISORIA_FAIXA_SENTIDO_DIFERENTE
         else:
-            print("ERRO: Tipo de faixa irreconhecivel " +
+            dprint(5)
+            eprint("Tipo de faixa irreconhecivel: " +
                   str(faixa_anterior.tipo) + " " + str(faixa_proxima.tipo))
             exit(1)
 
-    def draw_divisoria(self,  dlt: float, scr: pygame.Surface, faixa_anterior: Faixa, faixa_proxima: Faixa, direcao: direcao) -> float:
+    def draw_divisoria(self,  dlt: float, scr: pygame.Surface, faixa_anterior: Faixa, faixa_proxima: Faixa, direcao: Direcao) -> float:
         cor = self.get_cor_divisoria(faixa_anterior, faixa_proxima)
 
         dlt = dlt + LARGURA_DIVISORIA
@@ -201,6 +196,18 @@ class PistaDrawer(DrawItem):
         pygame.draw.rect(scr, cor, rect)
 
         return dlt
+
+    def draw_faixa(self, dlt: float, scr: pygame.Surface, faixa: Faixa) -> float:
+        cor = COR_FAIXA
+        if faixa.tipo == FaixaTipo["acostamento"]:
+            cor = COR_ACOSTAMENTO
+
+        # botar setas pra indicar direção?
+
+        rect = get_params_directional_rect(
+            self.pista.p1, self.pista.p2, faixa.sentido, dlt)
+
+        pygame.draw.rect(scr, cor, rect)
 
 
 def read_and_parse_json_file(path="config.json"):
@@ -217,23 +224,20 @@ def execute():
     for pista in data["pistas"]:
         faixas = []
         for faixa in pista["faixas"]:
-            faixa_obj = Faixa(faixa["tipo"], faixa["relativo"], faixa["sentido"])
+            faixa_obj = Faixa(
+                FaixaTipo[faixa["tipo"]], Direcao[faixa["relativo"]], Direcao[faixa["sentido"]])
             faixas.append(faixa_obj)
 
         pista = Pista(
             p1=pista["p1"],
             p2=pista["p2"],
-            direcao=pista["direcao"],
+            direcao=Direcao[pista["direcao"]],
             faixas=faixas
         )
 
         pistas.append(pista)
-    
-    gui = GUI()
 
-    gui.drawer.add_draw_item(PistaDrawer(pista))
-
-    gui.init()
+    gui = GUI(Drawer([PistaDrawer(pista)]))
     gui.run()
 
 
